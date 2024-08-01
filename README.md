84cfde164134feae9e3affeec86d4aef2f52dc64
liorking_microsoft <liorking@microsoft.com>
2024-08-01 12:44:42 +0300

    main

--- README.md
+++ README.md
@@ -1,18 +1,58 @@
-# Installation
+# Azure OpenAI File Translator
+
+This command line program translates PDF and TXT files into any target language using Azure OpenAI LLMs.
+
+## Important notes
+- Speed:
+   - Translation generates a lot of output tokens (unlike summarization for example). 
+   - Note that output tokens are processed slower and are more expensive than input tokens.
+- Token generation limitation:
+   - The context window of gpt-4 and gpt-4o is 128k tokens for input but only 4096 tokens for output.
+   - Translating 4096 tokens in English to a different language (like Chinese) might produce more than 4096 tokens in the output because it is in a different language.
+- Working in chunks:
+   - To avoid “chopping” the output because of the output limitations described above, I'm calling Azure OpenAI in chunks of sentences that can not exceed `CHUNK_SIZE` tokens(configurable with an environment variable).
+   - Each chunk contains whole sentences and is translated individually.
+   - The program concatenates the translation results of each chunk to the final output response.
+- gpt-4o vs. gpt-4o-mini:
+   - Gpt-4o-mini is much cheaper and faster than gpt-4o.
+   - The translation quality might NOT be as good as the quality of gpt-4o. That depends on the target language. I recommend to check which model works best for your use case.
+   - For gpt-4o-mini, the output window is 16K (unlike gpt-4o where the window is 4K). Therefore, you can increase the chunk size if you wish.
+- The program uses the tokenizer of `gpt-4o` and `gpt-4o-mini` to count the tokens. **DO NOT USE ANY OTHER MODEL**.
+
+## Installation
 1. Use Python 3.9 or higher.
 2. Perform the following:
    ```bash
    pip install -r requirement.txt
    ```
+ 
+### Environment parameters
 
-3. Set the parameter in the `.env` file for your environment.
+Set the following parameter in the `.env` file for your environment:
 
-# Use the application
+- `OPENAI_DEPLOYMENT_NAME` - The model's deployment name on Azure OpenAI service.
+- `AZURE_OPENAI_ENDPOINT` - The Azure OpenAI resource endpoint (URL) 
+- `OPENAI_API_KEY` - The Azure OpenAI resource key
+- `OPENAI_API_VERSION` - The OpenAI API version used
+- `DEST_LANG` - The destination language to which you wish to translate file content (For example: "French").
+- `INPUT_FILE` - Source file name
+- `OUTPUT_FILE` - Target file name that will contain the translated text
+- `PROMPT` - The prompt sent to Azure OpenAI endpoint.
+
+   Example for setting the `PROMPT` parameter:
+   ```
+   PROMPT="Translate the following text to {dest_lang}:\n\n{content}"
+   ```
+
+- `CHUNK_SIZE` sets the number of maximum tokens to send OpenAI in each iteration.
+
+## How to use the application
 The program accepts the following arguments:
 - `--dest_lang` - The destination language
 - `--model` - The OpenAI model to use (set to the deployment name)
 - `--input_file` - The file to translate
 - `--output_file` - The output file name
 
-If the arguments are not set via command line, the program will use the settings in the `.env` file.
-If the arguments are not set via command line and not set in the `.env` file, the program will ask the user to enter the missing arguments.
+If any argument is not set via command line, the program will use the settings in the `.env` file.\
+If any argument is not set via command line and not set in the `.env` file, the program will ask the user to enter the missing argument.
+
--- translate_file.py
+++ translate_file.py
@@ -4,45 +4,78 @@ from dotenv import load_dotenv
 from openai import AzureOpenAI
 import PyPDF2
 import docx
+import tiktoken
+import re
 
-def translate_content(client, content, dest_lang, model):
+# Using the tokenizer of gpt-4o and gpt-4o-mini for counting tokens
+tokenizer = tiktoken.get_encoding("o200k_base")
+
+client = AzureOpenAI(
+    azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+    api_key=os.getenv("OPENAI_API_KEY"),
+    api_version=os.getenv("OPENAI_API_VERSION")
+)
+        
+def count_tokens(text):
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+def split_text_into_chunks(text, max_chunk_size):
+    # Split the text into sentences
+    sentences = re.split(r'(?<=[.!?]) +', text)
+    
+    chunks = []
+    current_chunk = ""
+    
+    for sentence in sentences:
+        # Check if adding the next sentence would exceed the max_chunk_size
+        if count_tokens(current_chunk) + count_tokens(sentence) <= max_chunk_size:
+#        if len(current_chunk) + len(sentence) + 1 <= max_chunk_size:
+            current_chunk += sentence + " "
+        else:
+            if current_chunk:
+                chunks.append(current_chunk.strip())
+ 
+            current_chunk = sentence + " "
+    
+    # Add the last chunk if it's not empty
+    if current_chunk:
+        chunks.append(current_chunk.strip())
+    
+    return chunks
+
+def call_openai(content, dest_lang, model):
     PROMPT = os.getenv("PROMPT")
     prompt= PROMPT.format(content=content, dest_lang=dest_lang)
     
     response = client.chat.completions.create(
         model=model,
+        max_tokens=4096,
         messages=[
             {"role": "user", "content": prompt}
         ]
     )
     return response.choices[0].message.content
 
-def process_pdf(pdf_file, client, dest_lang, model):
+def translate_content(content, dest_lang, model, chunk_size=1000):
+    chunks = split_text_into_chunks (content, chunk_size)
+    chunk_num = 1
+    translate_text = ''
+    for chunk in chunks:
+        print (f"Translating chunk no. {chunk_num}")
+        translate_text += call_openai(chunk, dest_lang, model)+"\n"
+        chunk_num+=1
+    return translate_text
+       
+def process_pdf(pdf_file):
     with open(pdf_file, 'rb') as file:
         pdf_reader = PyPDF2.PdfReader(file)
-        translate_text = ''
+        content=''
         for page_num in range(len(pdf_reader.pages)):
-            print (f"Translating page no. {page_num+1}")
             page = pdf_reader.pages[page_num] 
-            content = page.extract_text()
-            translate_text += translate_content(client, content, dest_lang, model)+"\n"
-    
-    return translate_text
+            content += page.extract_text()
 
-def process_docx(docx_file, client, dest_lang, model):
-    # Open the Word file
-    doc = docx.Document(docx_file)
-    
-    # Extract the text from each paragraph
-    translate_text = ''
-    paragraph_no = 1
-    for paragraph in doc.paragraphs:
-        print (f"Translating paragraph no. {paragraph_no}")
-        translate_text += translate_content(client, paragraph.text, dest_lang, model)+"\n"
-        paragraph_no += 1
-        
-
-    return translate_text
+    return content
 
 def get_argument_or_env(args, arg_name, env_name):
     value = getattr(args, arg_name)
@@ -55,9 +88,9 @@ def get_argument_or_env(args, arg_name, env_name):
 def main():
     load_dotenv()
 
-    parser = argparse.ArgumentParser(description="Translate file content using an OpenAI model.")
+    parser = argparse.ArgumentParser(description="Translate file content using an OpenAI model. Supports PDF and TXT files.")
     
-    parser.add_argument('--dest_lang', type=str, help='The destination language')
+    parser.add_argument('--dest_lang', type=str, help='The destination language code')
     parser.add_argument('--model', type=str, help='The OpenAI model to use')
     parser.add_argument('--input_file', type=str, help='The input file path')
     parser.add_argument('--output_file', type=str, help='The output file path')
@@ -71,35 +104,20 @@ def main():
     in_file = get_argument_or_env(args, 'input_file', 'INPUT_FILE')
     out_file = get_argument_or_env(args, 'output_file', 'OUTPUT_FILE')
     
-    client = AzureOpenAI(
-        azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
-        api_key=os.getenv("OPENAI_API_KEY"),
-        api_version=os.getenv("OPENAI_API_VERSION")
-    )
-        
+    chunk_size = int(os.getenv("CHUNK_SIZE"))
+
     # Read the content of the input file
     file_extension = os.path.splitext(in_file)[1]
     if file_extension == '.pdf':
-        content = process_pdf(in_file, client, dest_lang, model)
-    elif file_extension == '.docx':
-        # Read the content of the docx file
-        content = process_docx(in_file, client, dest_lang, model)
+        content = process_pdf(in_file)
     elif file_extension == '.txt':
         with open(in_file, 'r') as file:
             content = file.read()
     else:
         raise ValueError(f"Unsupported file extension: {file_extension}")
     
-    PROMPT = os.getenv("PROMPT")
-    prompt= PROMPT.format(content=content, dest_lang=dest_lang)
-    
-    response = client.chat.completions.create(
-        model=model,
-        messages=[ {"role": "user", "content": prompt}]
-    )
-   
-    translated_text = response.choices[0].message.content
-    
+    translated_text = translate_content(content, dest_lang, model, chunk_size)
+
     with open(out_file, 'w') as file:
         file.write(translated_text)
     
